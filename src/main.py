30ed9dcded3afee6faed5b7ed02da5c9fcf3a68a
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2025-02-11 20:16:31 +0000

    Testing basic boto func

--- src/main.py
+++ src/main.py
@@ -1,11 +1,31 @@
+import boto3
 import json
+import pandas as pd
 from typeguard import typechecked
-from typing import Any
+from urllib.parse import urlparse
+
+def is_valid_s3_uri(uri: str) -> bool:
+    return bool((parsed := urlparse(uri)).scheme == 's3' 
+                and parsed.netloc 
+                and parsed.path 
+                and parsed.path.endswith('.csv'))
 
 @typechecked
-def json_checker(obfuscation_config: str):
+def json_checker(obfuscation_config: str) -> bool:
     try:
-        json.loads(obfuscation_config)
-        return True
+        config = json.loads(obfuscation_config)
     except json.JSONDecodeError:
         raise
+    keys = list(config.keys())
+    if not keys[0] == 'file_to_obfuscate' or not keys[1] == 'pii_fields':
+        raise ValueError
+    if not is_valid_s3_uri(config['file_to_obfuscate']):
+        raise ValueError("Invalid S3 URI")
+    return True
+
+def load_csv():
+    s3 = boto3.client('s3')
+    response = s3.get_object(Bucket='csv-test-11022025', Key='file.csv')
+    df = pd.read_csv(response['Body'])
+    df.head()
+    
--- test/test_main.py
+++ test/test_main.py
@@ -3,24 +3,37 @@ import pytest
 from src.main import json_checker
 from typeguard import TypeCheckError
 
-def test_json_checker_no_args():
-    with pytest.raises(TypeError, match=r"missing 1 required positional argument: 'obfuscation_config'"):
-        json_checker()
+class TestJSON:
+    def test_json_checker_no_args(self):
+        with pytest.raises(TypeError):
+            json_checker()
 
-@pytest.mark.parametrize("bad_arg", [
-    [1, 2, 3],
-    (1, 2, 3),
-    42,
-])
-def test_detect_incorrect_data_type_argument(bad_arg):
-    with pytest.raises(TypeCheckError):
-        json_checker(bad_arg)
+    @pytest.mark.parametrize("bad_arg", [
+        [1, 2, 3],
+        (1, 2, 3),
+        42,
+    ])
+    def test_detect_incorrect_data_type_argument(self, bad_arg):
+        with pytest.raises(TypeCheckError):
+            json_checker(bad_arg)
 
-def test_valid_json():
-    valid = '{"file_to_obfuscate": "s3://your_bucket/path/file.csv", "pii_fields": ["name", "email_address"]}'
-    assert json_checker(valid)
+    def test_valid_json(self):
+        valid = '{"file_to_obfuscate": "s3://your_bucket/path/file.csv", "pii_fields": ["name", "email_address"]}'
+        assert json_checker(valid)
 
-def test_malformed_json():
-    malformed = '{"file_to_obfuscate": "s3://your_bucket/path/file.csv", "pii_fields": ["name", "email_address"]'
-    with pytest.raises(json.decoder.JSONDecodeError):
-        json_checker(malformed)
+    def test_malformed_json(self):
+        malformed = '{"file_to_obfuscate": "s3://your_bucket/path/file.csv", "pii_fields": ["name", "email_address"]'
+        with pytest.raises(json.decoder.JSONDecodeError):
+            json_checker(malformed)
+
+    def test_incorrect_json_keys(self):
+        malformed = '{"foo": "s3://your_bucket/path/file.csv", "bar": ["name", "email_address"]}'
+        with pytest.raises(ValueError):
+            json_checker(malformed)
+
+    def test_malformed_s3_uri(self):
+        http_object = '{"file_to_obfuscate": "https://your_bucket/path/file.csv", "pii_fields": ["name", "email_address"]}'
+        with pytest.raises(ValueError):
+            json_checker(http_object)
+
+
