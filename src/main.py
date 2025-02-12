f03bfc3efc95511db28207f87d02eb6973e71376
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2025-02-12 19:43:10 +0000

    Implement CSV loading and saving functionality with sample data

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import boto3
+import io
 import json
 import pandas as pd
 from typeguard import typechecked
@@ -27,5 +28,16 @@ def load_csv():
     s3 = boto3.client('s3')
     response = s3.get_object(Bucket='csv-test-11022025', Key='file.csv')
     df = pd.read_csv(response['Body'])
-    df.head()
-    
+    return df
+
+def main():
+    df = load_csv()
+    df['name'] = '*****'
+    print(df.head())
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False)
+    s3 = boto3.client('s3')
+    s3.put_object(Bucket='csv-test-11022025', Key='file.csv', Body=csv_buffer.getvalue())    
+
+if __name__ == '__main__':
+    main()
